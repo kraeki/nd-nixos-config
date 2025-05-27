f8d7dc5ea857222458108a45b5e2f6b0a8b7fb9e
Andreas Schmid <ikeark@gmail.com>
2025-05-28 01:57:07 +0200

    Install dracula theme through nix home manager

--- home/kraeki/home.nix
+++ home/kraeki/home.nix
@@ -33,5 +33,37 @@
       [[ -f ~/.p10k.zsh ]] && source ~/.p10k.zsh
     '';
   };
+
+  home.packages = with pkgs; [ dracula-theme ];
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Dracula";
+      package = pkgs.dracula-theme;
+    };
+    iconTheme = {
+      name = "Dracula";
+      package = pkgs.dracula-theme;
+    };
+    cursorTheme = {
+      name = "Dracula";
+      package = pkgs.dracula-theme;
+    };
+    
+    gtk3.extraConfig = {
+      "gtk-application-prefer-dark-theme" = "1";
+    };
+    gtk4.extraConfig = {
+      "gtk-application-prefer-dark-theme" = "1";
+    };
+  };
+
+  # Ensure GTK settings are applied for Wayland
+  home.pointerCursor = {
+    gtk.enable = true;
+    package = pkgs.dracula-theme;
+    name = "Dracula-cursors";
+    size = 24; # Adjust size as needed
+  };
 }
 
